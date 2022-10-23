aa5281240d406947f8315551afb06a81adb398ef
Maksim <mackenneth@yandex.ru>
2022-10-23 14:26:48 +0300

    feat add table displaying

---
+++ src/components/EmployeesTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-table-simple
+    striped
+    bordered
+    hover
+    responsive
+    small
+    :itemes="employees"
+  >
+    <b-thead>
+      <b-th>
+        Имя
+      </b-th>
+      <b-th>
+        Телефон
+      </b-th>
+    </b-thead>
+    <b-tbody>
+      <b-tr
+        v-for="(employee, index) in employees"
+        :key="index"
+      >
+        <b-td class="text-start">
+          <template v-for="item in employee.nestingLevel">
+            -
+          </template>
+          {{ employee.firstName }}
+        </b-td>
+        <b-td>
+          {{ employee.phoneNumber }}
+        </b-td>
+      </b-tr>
+    </b-tbody>
+  </b-table-simple>
+</template>
+
+<script>
+import { BTableSimple, BThead, BTbody, BTd, BTr, BTh } from 'bootstrap-vue'
+
+export default {
+  name: 'EmployeesTable',
+  components: {
+    BTableSimple,
+    BThead,
+    BTbody,
+    BTd,
+    BTr,
+    BTh
+  },
+  props: {
+    employees: {
+      type: Array,
+      required: true,
+      default: () => ([])
+    }
+  },
+}
+</script>
